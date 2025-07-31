1ca4189ae5fc797c64ff94ecf0e1877a34b737d8
Serkan <14278530+SergeantSerk@users.noreply.github.com>
2025-07-31 09:45:06 +0100

    Update main.cpp

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -169,6 +169,7 @@ int main(int argc, char** argv)
   server::ServerInstance serverInstance(resourceDirectory);
   serverInstance.Initialize();
 
+  spdlog::info("Test etc blah blah new code");
   spdlog::info(
     "Server started up in {}ms",
     std::chrono::duration_cast<std::chrono::milliseconds>(
