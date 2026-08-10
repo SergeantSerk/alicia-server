d0731467436a07de1ec8b5fb531aebe36315636b
Serkan <14278530+SergeantSerk@users.noreply.github.com>
2026-08-10 11:16:31 +0100

    Guild command cleanup

--- src/server/lobby/LobbyNetworkHandler.cpp
+++ src/server/lobby/LobbyNetworkHandler.cpp
@@ -3072,7 +3072,7 @@ void LobbyNetworkHandler::HandleLeaveGuildParty(
 
 void LobbyNetworkHandler::HandleGuildPartyList(
   ClientId clientId,
-  [[maybe_unused]] const protocol::AcCmdCLGuildPartyList& command)
+  const protocol::AcCmdCLGuildPartyList&)
 {
   const auto& clientContext = GetClientContext(clientId);
 
@@ -3377,7 +3377,7 @@ void LobbyNetworkHandler::HandleStartGuildPartyMatch(
 
 void LobbyNetworkHandler::HandleStopGuildPartyMatch(
   ClientId clientId,
-  [[maybe_unused]] const protocol::AcCmdCLStopGuildPartyMatch& command)
+  const protocol::AcCmdCLStopGuildPartyMatch&)
 {
   const auto& clientContext = GetClientContext(clientId);
 
